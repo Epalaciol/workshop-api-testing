5dbc49a45cc54892eaea8d491df496878bee2446
Esteban Palacio Londoño <esteban.1054@hotmail.com>
2022-07-07 19:19:03 -0500

    Add eslint set-up (#5)

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -3,82 +3,75 @@ const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
 describe('First Api Tests', () => {
-    it('Consume GET service', async () => {
-        const response = await axios.get('https://httpbin.org/ip');
+  it('Consume GET service', async () => {
+    const response = await axios.get('https://httpbin.org/ip');
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data).to.have.property('origin');
-    })
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property('origin');
+  });
 
-    it('Consume GET Service with query parameters', async () => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+  it('Consume GET Service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-        const response = await axios.get('https://httpbin.org/get', {query});
+    const response = await axios.get('https://httpbin.org/get', { query });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.query).to.eql(query);
-    })
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.query).to.eql(query);
+  });
 
-    it('Consume POST service with query parameters', async() => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+  it('Consume POST service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-        const response = await axios.post('https://httpbin.org/post', {query});
+    const response = await axios.post('https://httpbin.org/post', { query });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.not.be.null;
-        expect(response.data.json.query).to.eql(query);
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json.query).to.eql(query);
+  });
 
-    })
+  it('Consume PATCH service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-    it('Consume PATCH service with query parameters', async() => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+    const response = await axios.patch('https://httpbin.org/patch', { query });
 
-        const response = await axios.patch('https://httpbin.org/patch', {query});
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json.query).to.eql(query);
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.not.be.null;
-        expect(response.data.json.query).to.eql(query);
+  it('Consume PUT service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-    })
+    const response = await axios.put('https://httpbin.org/put', { query });
 
-    it('Consume PUT service with query parameters', async() => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json.query).to.eql(query);
+  });
 
-        const response = await axios.put('https://httpbin.org/put', {query});
+  it('Consume DELETE service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.not.be.null;
-        expect(response.data.json.query).to.eql(query);
+    const response = await axios.delete('https://httpbin.org/delete', { query });
 
-    })
-
-    it('Consume DELETE service with query parameters', async() => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
-
-        const response = await axios.delete('https://httpbin.org/delete', {query});
-
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.be.null;
-        expect(response.data.data).to.be.empty;
-    })
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.data).to.be.eql('');
+  });
 });
